e327d1e2567c78d8aa34cbf55d9d49d8d86d239c
thọ thọ <thovvph31698@fpt.edu.vn>
2023-11-01 16:49:35 +0700

    Add project files.

---
+++ GameBauCua/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace GameBauCua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lb_tom = new System.Windows.Forms.Label();
+            this.lb_hieu = new System.Windows.Forms.Label();
+            this.lb_ga = new System.Windows.Forms.Label();
+            this.lb_ca = new System.Windows.Forms.Label();
+            this.lb_cua = new System.Windows.Forms.Label();
+            this.lb_bau = new System.Windows.Forms.Label();
+            this.ptb_Cua = new System.Windows.Forms.PictureBox();
+            this.ptb_Ga = new System.Windows.Forms.PictureBox();
+            this.ptb_Hieu = new System.Windows.Forms.PictureBox();
+            this.ptb_Tom = new System.Windows.Forms.PictureBox();
+            this.ptb_Ca = new System.Windows.Forms.PictureBox();
+            this.ptb_Bau = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ptb_R1 = new System.Windows.Forms.PictureBox();
+            this.ptb_R3 = new System.Windows.Forms.PictureBox();
+            this.ptb_R2 = new System.Windows.Forms.PictureBox();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Cua)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ga)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Hieu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Tom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Bau)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lb_tom);
+            this.groupBox1.Controls.Add(this.lb_hieu);
+            this.groupBox1.Controls.Add(this.lb_ga);
+            this.groupBox1.Controls.Add(this.lb_ca);
+            this.groupBox1.Controls.Add(this.lb_cua);
+            this.groupBox1.Controls.Add(this.lb_bau);
+            this.groupBox1.Controls.Add(this.ptb_Cua);
+            this.groupBox1.Controls.Add(this.ptb_Ga);
+            this.groupBox1.Controls.Add(this.ptb_Hieu);
+            this.groupBox1.Controls.Add(this.ptb_Tom);
+            this.groupBox1.Controls.Add(this.ptb_Ca);
+            this.groupBox1.Controls.Add(this.ptb_Bau);
+            this.groupBox1.Location = new System.Drawing.Point(47, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(527, 259);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Vùng đặt cược";
+            // 
+            // lb_tom
+            // 
+            this.lb_tom.AutoSize = true;
+            this.lb_tom.Location = new System.Drawing.Point(425, 118);
+            this.lb_tom.Name = "lb_tom";
+            this.lb_tom.Size = new System.Drawing.Size(13, 13);
+            this.lb_tom.TabIndex = 16;
+            this.lb_tom.Text = "0";
+            // 
+            // lb_hieu
+            // 
+            this.lb_hieu.AutoSize = true;
+            this.lb_hieu.Location = new System.Drawing.Point(425, 231);
+            this.lb_hieu.Name = "lb_hieu";
+            this.lb_hieu.Size = new System.Drawing.Size(13, 13);
+            this.lb_hieu.TabIndex = 15;
+            this.lb_hieu.Text = "0";
+            // 
+            // lb_ga
+            // 
+            this.lb_ga.AutoSize = true;
+            this.lb_ga.Location = new System.Drawing.Point(244, 231);
+            this.lb_ga.Name = "lb_ga";
+            this.lb_ga.Size = new System.Drawing.Size(13, 13);
+            this.lb_ga.TabIndex = 14;
+            this.lb_ga.Text = "0";
+            // 
+            // lb_ca
+            // 
+            this.lb_ca.AutoSize = true;
+            this.lb_ca.Location = new System.Drawing.Point(68, 231);
+            this.lb_ca.Name = "lb_ca";
+            this.lb_ca.Size = new System.Drawing.Size(13, 13);
+            this.lb_ca.TabIndex = 13;
+            this.lb_ca.Text = "0";
+            // 
+            // lb_cua
+            // 
+            this.lb_cua.AutoSize = true;
+            this.lb_cua.Location = new System.Drawing.Point(244, 116);
+            this.lb_cua.Name = "lb_cua";
+            this.lb_cua.Size = new System.Drawing.Size(13, 13);
+            this.lb_cua.TabIndex = 12;
+            this.lb_cua.Text = "0";
+            // 
+            // lb_bau
+            // 
+            this.lb_bau.AutoSize = true;
+            this.lb_bau.Location = new System.Drawing.Point(68, 116);
+            this.lb_bau.Name = "lb_bau";
+            this.lb_bau.Size = new System.Drawing.Size(13, 13);
+            this.lb_bau.TabIndex = 11;
+            this.lb_bau.Text = "0";
+            // 
+            // ptb_Cua
+            // 
+            this.ptb_Cua.Location = new System.Drawing.Point(213, 33);
+            this.ptb_Cua.Name = "ptb_Cua";
+            this.ptb_Cua.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Cua.TabIndex = 10;
+            this.ptb_Cua.TabStop = false;
+            this.ptb_Cua.Click += new System.EventHandler(this.ptb_Cua_Click);
+            // 
+            // ptb_Ga
+            // 
+            this.ptb_Ga.Location = new System.Drawing.Point(213, 155);
+            this.ptb_Ga.Name = "ptb_Ga";
+            this.ptb_Ga.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Ga.TabIndex = 9;
+            this.ptb_Ga.TabStop = false;
+            this.ptb_Ga.Click += new System.EventHandler(this.ptb_Ga_Click);
+            // 
+            // ptb_Hieu
+            // 
+            this.ptb_Hieu.Location = new System.Drawing.Point(392, 155);
+            this.ptb_Hieu.Name = "ptb_Hieu";
+            this.ptb_Hieu.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Hieu.TabIndex = 8;
+            this.ptb_Hieu.TabStop = false;
+            this.ptb_Hieu.Click += new System.EventHandler(this.ptb_Hieu_Click);
+            // 
+            // ptb_Tom
+            // 
+            this.ptb_Tom.Location = new System.Drawing.Point(392, 33);
+            this.ptb_Tom.Name = "ptb_Tom";
+            this.ptb_Tom.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Tom.TabIndex = 7;
+            this.ptb_Tom.TabStop = false;
+            this.ptb_Tom.Click += new System.EventHandler(this.ptb_Tom_Click);
+            // 
+            // ptb_Ca
+            // 
+            this.ptb_Ca.Location = new System.Drawing.Point(33, 155);
+            this.ptb_Ca.Name = "ptb_Ca";
+            this.ptb_Ca.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Ca.TabIndex = 6;
+            this.ptb_Ca.TabStop = false;
+            this.ptb_Ca.Click += new System.EventHandler(this.ptb_Ca_Click);
+            // 
+            // ptb_Bau
+            // 
+            this.ptb_Bau.Location = new System.Drawing.Point(33, 33);
+            this.ptb_Bau.Name = "ptb_Bau";
+            this.ptb_Bau.Size = new System.Drawing.Size(79, 73);
+            this.ptb_Bau.TabIndex = 0;
+            this.ptb_Bau.TabStop = false;
+            this.ptb_Bau.Click += new System.EventHandler(this.ptb_Bau_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ptb_R1);
+            this.groupBox2.Controls.Add(this.ptb_R3);
+            this.groupBox2.Controls.Add(this.ptb_R2);
+            this.groupBox2.Location = new System.Drawing.Point(59, 277);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(503, 153);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Vùng tung xúc xắc";
+            // 
+            // ptb_R1
+            // 
+            this.ptb_R1.Location = new System.Drawing.Point(62, 37);
+            this.ptb_R1.Name = "ptb_R1";
+            this.ptb_R1.Size = new System.Drawing.Size(79, 73);
+            this.ptb_R1.TabIndex = 5;
+            this.ptb_R1.TabStop = false;
+            // 
+            // ptb_R3
+            // 
+            this.ptb_R3.Location = new System.Drawing.Point(338, 37);
+            this.ptb_R3.Name = "ptb_R3";
+            this.ptb_R3.Size = new System.Drawing.Size(79, 73);
+            this.ptb_R3.TabIndex = 3;
+            this.ptb_R3.TabStop = false;
+            // 
+            // ptb_R2
+            // 
+            this.ptb_R2.Location = new System.Drawing.Point(202, 37);
+            this.ptb_R2.Name = "ptb_R2";
+            this.ptb_R2.Size = new System.Drawing.Size(79, 73);
+            this.ptb_R2.TabIndex = 4;
+            this.ptb_R2.TabStop = false;
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Location = new System.Drawing.Point(599, 347);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(108, 40);
+            this.btn_Play.TabIndex = 2;
+            this.btn_Play.Text = "Play";
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(610, 53);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1000Đ";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(610, 88);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2000Đ";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(610, 128);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(63, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "10000Đ";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Số tiền hiện tại";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(619, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 442);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btn_Play);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Cua)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ga)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Hieu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Tom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Ca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_Bau)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptb_R2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox ptb_Cua;
+        private System.Windows.Forms.PictureBox ptb_Ga;
+        private System.Windows.Forms.PictureBox ptb_Hieu;
+        private System.Windows.Forms.PictureBox ptb_Tom;
+        private System.Windows.Forms.PictureBox ptb_Ca;
+        private System.Windows.Forms.PictureBox ptb_Bau;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox ptb_R1;
+        private System.Windows.Forms.PictureBox ptb_R3;
+        private System.Windows.Forms.PictureBox ptb_R2;
+        private System.Windows.Forms.Button btn_Play;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lb_tom;
+        private System.Windows.Forms.Label lb_hieu;
+        private System.Windows.Forms.Label lb_ga;
+        private System.Windows.Forms.Label lb_ca;
+        private System.Windows.Forms.Label lb_cua;
+        private System.Windows.Forms.Label lb_bau;
+    }
+}
+
---
+++ GameBauCua/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameBauCua
+{
+    public partial class Form1 : Form
+    {
+        int limit = 0;
+        public Form1()
+        {
+            InitializeComponent(); // khởi tạo các controll
+            // Set ánh cho pictrubox
+            ptb_Bau.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\bầu.jpg");
+            ptb_Bau.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Cua.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\cua.jpg");
+            ptb_Cua.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Tom.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\tôm.jpg");
+            ptb_Tom.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Ca.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\cá.jpg");
+            ptb_Ca.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Ga.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\gà.jpg");
+            ptb_Ga.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_Hieu.Image = Image.FromFile(@"E:\ảnh nền\Bầu cua tôm cá\hiêu.jpg");
+            ptb_Hieu.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            // Tạo 1 mảng chứa các đường dẫn đến ảnh
+            string[] arrBauCua = new string[]
+            {
+                @"E:\ảnh nền\Bầu cua tôm cá\bầu.jpg",
+                @"E:\ảnh nền\Bầu cua tôm cá\cua.jpg",
+                @"E:\ảnh nền\Bầu cua tôm cá\tôm.jpg",
+                @"E:\ảnh nền\Bầu cua tôm cá\cá.jpg",
+                @"E:\ảnh nền\Bầu cua tôm cá\gà.jpg",
+                @"E:\ảnh nền\Bầu cua tôm cá\hiêu.jpg",
+            };
+            // Ramdom vị trí link ảnh trong mảng trên để gán cho pictrubox
+            Random ramdom = new Random();
+            int ramdom1 = ramdom.Next(0, 5); int ramdom2 = ramdom.Next(0,5); int ramdom3 = ramdom.Next(0,5);
+            // Gản ảnh cho pictureBox thông qua URL 
+            ptb_R1.Image = Image.FromFile(arrBauCua[ramdom1]);
+            ptb_R1.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_R2.Image = Image.FromFile(arrBauCua[ramdom2]);
+            ptb_R2.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptb_R3.Image = Image.FromFile(arrBauCua[ramdom3]);
+            ptb_R3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+        public void ptb_Click(Label lb)
+        {
+            lb.Text = Convert.ToInt32(lb.Text) + 1 + "";
+            limit++;    
+            if (limit > 3)
+            {
+                limit -= Convert.ToInt32(lb.Text);
+                lb.Text = "0";
+            }
+        }
+       
+        public void TotaBet()
+        {
+            if(lb_bau.Text !="0" ) check[0] = Convert.ToInt32(lb_bau.Text);
+            if(lb_cua.Text !="0" ) check[0] = Convert.ToInt32(lb_cua.Text);
+            if(lb_tom.Text !="0" ) check[0] = Convert.ToInt32(lb_tom.Text);
+            if(lb_ca.Text !="0" ) check[0] = Convert.ToInt32(lb_ca.Text);
+            if(lb_ga.Text !="0" ) check[0] = Convert.ToInt32(lb_ga.Text);
+            if(lb_hieu.Text !="0" ) check[0] = Convert.ToInt32(lb_hieu.Text);
+            MessageBox.Show(string.Join("-", check));
+            // Tính tổng số lượng đặt cược
+            int count = 0;
+            foreach (var item in check) count += item;
+            TotaBet = count * bet
+        }
+
+        private void ptb_Bau_Click(object sender, EventArgs e)
+        {
+            ptb_Click(lb_bau);
+        }
+
+        private void ptb_Hieu_Click(object sender, EventArgs e)
+        {
+            ptb_Click(lb_hieu);
+        }
+
+        private void ptb_Cua_Click(object sender, EventArgs e)
+        {
+            ptb_Click( lb_cua);
+        }
+
+        private void ptb_Tom_Click(object sender, EventArgs e)
+        {
+            ptb_Click(lb_tom);
+        }
+
+        private void ptb_Ca_Click(object sender, EventArgs e)
+        {
+            ptb_Click( lb_ca);
+        }
+
+        private void ptb_Ga_Click(object sender, EventArgs e)
+        {
+            ptb_Click( lb_ga);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
